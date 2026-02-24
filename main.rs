bba85804840577b1fa7163874ad149fc4b9d4c66
Jay Telford <hello@jaytelford.me>
2026-02-24 16:27:01 +0000

    init commit

---
+++ main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut message = "Hello, world!";
+
+    println!("{}", message);
+    message = "Hi there";
+    println!("Some text: {}", message);
+
+    let mut age = 10;
+    println!("{}", age);
+    age = 30;
+    println!("{}", age);
+}
+
+
+
+// binary executable code or library
+// LLVM --> binary
