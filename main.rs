0fa98acae2492e7af25af6922cb12c6bfbd2d1d1
Jay Telford <hello@jaytelford.me>
2026-02-26 17:54:42 +0000

    Section 3: Basic Types Lesson 10 - Primitive Types

--- main.rs
+++ main.rs
@@ -1,12 +1,20 @@
 fn main() {
-    let message = "Hello, world!";
-    let message_2 = print_welcome(message);
-    println!("{}", message_2);
-}
+    let os_it_fun = false;
+    let num = -10;
+    // i32 --> signed integer of 32bits
+    // --> signed can hold positive or negative values
 
-fn print_welcome(text: &str) -> &str {
-    println!("{}", text);
-    let new_message = "Hi There";
-    // return new_message;
-    new_message
+    let small_num: u8 = 255;
+    // u8 --> unsigned integer of 8bits
+    // 2^8 -1
+    // the bigger the u value the more bits it can hold, for example u32 can hold max of 2^32 -1
+
+    let small_num_2: i8 = 127;
+    // -2^7 --> 2^7 -1
+    // -128 --> 127
+
+    let sys_num: isize = -10;
+    let sys_numer_2: usize = 10;
+    // operating system related type
+    // the number of bits is determined by the system arch: e.g. 32bit or 64bit systems
 }
