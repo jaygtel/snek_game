0c49e2cca648cddd9c0e1c038f09c3f7802f99b4
Jay Telford <hello@jaytelford.me>
2026-03-07 16:23:01 +0000

    added new examples file

---
+++ examples.rs
@@ -0,0 +1,33 @@
+fn main() {
+    c();
+    d();
+    f();
+}
+
+fn a() {
+    println!)"calling A");
+    e();
+}
+
+fn b() {
+    pintln!("calling B")
+}
+
+fn c() {
+    println!("calling C")
+}
+
+fn d() {
+    println!("calling D");
+}
+
+fn e() {
+    println!("calling E");
+}
+
+fn f() {
+    println!("calling F");
+    b();
+}
+
+// xxd -g1 main
