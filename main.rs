ce9551b75175b0154b7c80858535b347d4d29523
Jay Telford <hello@jaytelford.me>
2026-03-07 15:59:58 +0000

    created dev branch

--- main.rs
+++ main.rs
@@ -1,18 +1,14 @@
 fn main() {
-    let _float_num: f32 = 3.14;
-    let _float_num_2: f64 = 3.2334327489;
+    let a = 10;
+    let b = a;
+    let c = 15;
+    let d = (a + b);
 
-    let tup: (i32, &str, u8) = (20, "Hello", 1);
+    println!("{}", d);
+}
 
-    println!("{}", tup.1);
-
-    let (a,_b,_c) = tup;
-    println!("{}", a);
-
-    let x = [1,5,6,7];
-
-    println!("{}", x[2]);
-
-    let y = [2; 6]; // you get an array of 6 items but only 2s e.g: [2, 2, 2, 2, 2, 2]
-    println!("{}", y[5]);
+fn add(x: u32, y: u32) -> u32 {
+    let sum = x + y;
+    // return sum
+    sum
 }
