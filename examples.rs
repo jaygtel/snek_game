28d43e8e2fe81b2bd383f75bd3280e100ee61871
Jay Telford <hello@jaytelford.me>
2026-03-07 17:22:33 +0000

    Section 4: Move and Copy Lesson 14. AI More Stack Added comment to explain stack overflow errors.

--- examples.rs
+++ examples.rs
@@ -9,6 +9,33 @@ fn a() {
     e();
 }
 
+/*
+
+If we change fn a() to be as follows, then we will create a stack overflow
+
+fn a() {
+    println!("calling A");
+    d();
+}
+
+This is because:
+
+a() calls d()
+d() calls a()
+a() calls d() again
+d() calls a() again ... and so on endlessly
+
+Each function call uses a stack frame (memory that stores the function’s local variables and return
+address). Since the recursion never stops, the stack keeps growing until it runs out of memory.
+
+When the stack is exhausted, the program panics in Rust with a stack overflow. In most systems,
+this terminates the program immediately.
+
+It should be noted that this is a runtime error, not a compiling error.  Rust will allow you to
+compile this code, but when you try to run it, the program will crash due to a stack overflow.
+
+*/
+
 fn b() {
     println!("calling B");
 }
@@ -19,6 +46,7 @@ fn c() {
 
 fn d() {
     println!("calling D");
+    a();
 }
 
 fn e() {
