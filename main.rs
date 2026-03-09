a555ceb406923633a0412e6d38d3653cda1ef07e
Jay Telford <hello@jaytelford.me>
2026-03-09 16:28:50 +0000

    Section 4: Move and Copy Lesson 18 Return moved values

--- main.rs
+++ main.rs
@@ -1,15 +1,12 @@
 fn main() {
-    let message = String::from("Hello");  // message coming into the scope
-    print_message(message); // message is moved into the print_message function
-    // from this point forward message is no longer valid
-}
-// message is going out of the scope,
-// but nothing will happen because it was moved into print_message
+    let message = String::from("Hello");
+    let message = extend_message(message);
 
-fn print_message(a: String) { // a comes into the scope
-    println!("{}", a);
-    let c = a; // c is coming into the scope and a is moved into the c
-    // a is no longer valid
+    println!("{}", message);
 }
-// a is going out of the scope, but nothing more will happen because it was moved
-// c is going out of the scope  and 'drop' is called which clears the memory from the heap
+
+fn extend_message(mut a: String) -> String {
+
+    a.push_str(" World");
+    a
+}
