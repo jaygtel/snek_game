e1ebec7dbdc50060a12bb74923da75c6a7ae40cd
Jay Telford <hello@jaytelford.me>
2026-03-10 16:40:11 +0000

    Section 5L Reference & Borrowing Lesson 23. Dereference

--- main.rs
+++ main.rs
@@ -1,14 +1,7 @@
 fn main() {
-    let mut message = String::from("Hello");
-    let message_3 = &message;
-    printlin!("{}", message_3);
-    let message_2= &mut message;
+    let a = 10;
+    let b = &a;
+    let c = &b;
 
-    unpredictable_mutate(message_2);
-    println!("{}", message_2);
-
-}
-
-fn unpredictable_mutate(val: &mut Strong) {
-    val.push_str("_unpredictable");
+    println!("{}", a == **c);
 }
