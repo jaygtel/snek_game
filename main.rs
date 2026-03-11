3c04b140d10bfd911df8895f9e298d8d311e7e54
Jay Telford <hello@jaytelford.me>
2026-03-11 10:12:36 +0000

    Section 6: Strings Lesson 26. String slice

--- main.rs
+++ main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let message = String::from("Hello");
-    let name = "Filip";
+    let mut message = String::from("Hello");
+    let slice = &message[2..4]; // 2 -> 3
+    // H E L L O
+    // 0 1 2 3 4
 
-    message.push_str(" World");
-    // name.push_str(" Jerga");
+    println!("{}", slice);
 }
-
